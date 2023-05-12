8242a38df51ad1cec396d8c24f0d94beebb735ec
Max <max.f29@mail.ru>
2023-05-12 20:02:38 +0400

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './components/App/App';
-
-const container = document.querySelector('#root');
-const root = createRoot(container);
-
-root.render(<App />);
+import React from 'react';import { createRoot } from 'react-dom/client';import App from './components/App/App';const container = document.querySelector('#root');const root = createRoot(container);root.render(<App />);
